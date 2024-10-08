ad06df1fc9c26874f5bcd31c97cbc07726c438df
Tristan Jahnke <trjahnke@gmail.com>
2024-10-07 22:51:19 -0500

    feat: update and fix load errors (#2)
    * feat: remove defualts and fixed keymaps
    
    * feat: fix load issues and keymaps

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,24 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = function(_, opts)
+    local trouble = require("trouble")
+    local symbols = trouble.statusline({
+      mode = "lsp_document_symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      hl_group = "lualine_c_normal",
+    })
+    opts.sections = opts.sections or {}
+    opts.sections.lualine_c = opts.sections.lualine_c or {}
+    table.insert(opts.sections.lualine_c, {
+      symbols.get,
+      cond = symbols.has,
+    })
+    end,
+  config = function()
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
