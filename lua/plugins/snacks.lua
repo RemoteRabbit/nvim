8d513893b911a4639c560a7edc274f44e9d37799
Tristan Jahnke <trjahnke@protonmail.com>
2025-10-09 17:12:30 -0500

    refactor: updates to snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -20,12 +20,12 @@ return {
      --------------------------------------------------------------------------------------------------------
 ]],
         keys = {
-          { icon = "󰈞 ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = "🪄", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = "📰", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = "🗄️", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = "🔎", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           {
-            icon = " ",
+            icon = "⚙️",
             key = "c",
             desc = "Config",
             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
@@ -34,6 +34,21 @@ return {
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
+    },
+    dim = {
+      enabled = true,
+    },
+    -----
+    git = {
+      enabled = true,
+    },
+    -----
+    image = {
+      enabled = true,
+    },
+    -----
+    indent = {
+      enabled = true,
     },
     -----
     lazygit = {
@@ -63,6 +78,21 @@ return {
     },
   },
   keys = {
+    -- {
+    --   "<leader>st",
+    --   function()
+    --     Snacks.picker.todo_comments()
+    --   end,
+    --   desc = "Todo",
+    -- },
+    -- {
+    --   "<leader>sT",
+    --   function()
+    --     Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+    --   end,
+    --   desc = "Todo/Fix/Fixme",
+    -- },
+    --
     {
       "<leader>.",
       function()
@@ -218,6 +248,12 @@ return {
         Snacks.zen()
       end,
     },
+    {
+      "<leader>ee",
+      function()
+        Snacks.explorer.open()
+      end,
+    },
   },
   init = function()
     vim.api.nvim_create_autocmd("User", {
@@ -233,15 +269,12 @@ return {
 
         Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
         Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
         Snacks.toggle.diagnostics():map("<leader>ud")
-        Snacks.toggle.line_number():map("<leader>ul")
         Snacks.toggle
           .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
           :map("<leader>uc")
-        Snacks.toggle.treesitter():map("<leader>uT")
-        Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
         Snacks.toggle.inlay_hints():map("<leader>uh")
+        Snacks.toggle.dim({ enable = true }):map("<leader>uz")
       end,
     })
   end,
