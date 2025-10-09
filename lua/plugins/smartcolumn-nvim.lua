00b7d0634a7291870180c7025e453a8b776e5a01
Tristan Jahnke <trjahnke@protonmail.com>
2025-10-09 17:12:20 -0500

    feat: moved from column to smartcolumn setup

---
+++ lua/plugins/smartcolumn-nvim.lua
@@ -0,0 +1,14 @@
+return {
+  "m4xshen/smartcolumn.nvim",
+  colorcolumn = {
+    "80",
+    "120",
+  },
+  disabled_filetypes = {
+    "lazy",
+    "help",
+    "checkhealth",
+    "snacks_dashboard",
+  },
+  opts = {},
+}
