da18c8a0dce4066924541285816bb1a68fc71006
Tristan Jahnke <trjahnke@protonmail.com>
2025-10-09 17:11:31 -0500

    feat: Remove unused or migrated plugins

--- lua/plugins/vim-terraform-completion.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "juliosueiras/vim-terraform-completion",
-  ft = "terraform",
-  config = function()
-    vim.g.terraform_completion_keys = 1
-    vim.g.terraform_registry_module_completion = 1
-  end,
-}
--- lua/plugins/vim-visual-multi.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "mg979/vim-visual-multi",
-  config = function()
-    vim.g.VM_theme = "iceblue"
-    vim.g.VM_highlight_matches = "hi! Search gui=underline"
-  end,
-}
