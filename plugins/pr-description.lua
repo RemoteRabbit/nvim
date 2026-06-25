5a4e8a9c156f03aa048dabe2aded5be70432d842
Tristan Jahnke <trjahnke@protonmail.com>
2026-06-25 09:37:20 -0500

    fix: Add keymaps for pr-description
    Signed-off-by: Tristan Jahnke <trjahnke@protonmail.com>

--- plugins/pr-description.lua
+++ plugins/pr-description.lua
@@ -1,3 +1,14 @@
 return {
   src = "https://github.com/remoterabbit/pr-description.nvim",
+  config = function()
+    require("pr-description").setup({
+      foldable_file_changes = true,
+    })
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>gd",
+      ":PRDescription!<cr>",
+      { desc = "Generate PR Description from commits." }
+    )
+  end,
 }
